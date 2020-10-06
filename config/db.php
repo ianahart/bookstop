094267d7a8de8b83c82c4e0703f2cd4574562599
Ian Hart <ianhart7790@gmail.com>
2020-10-06 18:57:03 -0400

    fixed: public

--- config/db.php
+++ config/db.php
@@ -3,21 +3,26 @@
 
 // require "../vendor/autoload.php";
 
-include(dirname(__DIR__) . '/vendor/autoload.php');
+// include(dirname(__DIR__) . '/vendor/autoload.php');
 
-use Dotenv\Dotenv;
-
-
-
-
-
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// use Dotenv\Dotenv;
 
 
 
 
-$conn = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB']);
+
+// $dotenv = Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+
+$conn = mysqli_connect($server, $username, $password, $db);
 
 
 if (!$conn) {
