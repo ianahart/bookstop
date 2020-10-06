45ac72c42bbff9de2b472e8323c8d6a812d933be
Ian Hart <ianhart7790@gmail.com>
2020-10-06 18:21:16 -0400

    fixed relative paths in register.php

--- public/login.php
+++ public/login.php
@@ -1,6 +1,20 @@
 <?php
 session_start();
-include('../config/db.php');
+// include('../config/db.php');
+
+use Dotenv\Dotenv;
+
+
+
+
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+
+
+$conn = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB']);
 
 
 
