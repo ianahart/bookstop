e86f29cf1fe48ca147d615986071af33e76e63a3
Ian Hart <ianhart7790@gmail.com>
2020-10-06 19:18:58 -0400

    updating links

--- public/forgotpassword.php
+++ public/forgotpassword.php
@@ -4,7 +4,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-include(dirname(__DIR__) . '../vendor/autoload.php');
+include(dirname(__DIR__) . '/vendor/autoload.php');
 
 
 include(dirname(__DIR__) . '/config/db.php');
