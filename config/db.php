09050e0e76eb258993e473cdf8a39cd9e3e3254e
Ian Hart <ianhart7790@gmail.com>
2020-10-06 16:41:46 -0400

    added print to check connection

--- config/db.php
+++ config/db.php
@@ -16,8 +16,10 @@ $dotenv->load();
 
 
 $conn = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB']);
-
+print_r($_ENV);
 
 if (!$conn) {
   echo 'Connection Error: ' . mysqli_connect_error($conn);
+} else {
+  print_r("connected");
 }
