06b08d50a5804da5fc36a1c6351af594e2775275
Ian Hart <ianhart7790@gmail.com>
2020-10-06 22:15:18 -0400

    updating links

--- public/mybooks.php
+++ public/mybooks.php
@@ -1,5 +1,5 @@
 <?php
-ob_start();
+
 session_start();
 
 include(dirname(__DIR__) . '/config/db.php');
--- public/templates/account.php
+++ public/templates/account.php
@@ -1,6 +1,6 @@
 <?php
-
-include __DIR__ . '/config/db.php';
+session_start();
+include(dirname(__DIR__) . '/config/db.php');
 
 
 
